0306cf050fa60fe4b5916aa31649bc214bdddb91
chishu <linzhanyu.lzy@alibaba-inc.com>
2023-12-24 00:51:05 +0800

    upgrade

--- rule-tree/src/main/java/org/amumu/rule/tree/client/model/res/RuleTreeResultVO.java
+++ rule-tree/src/main/java/org/amumu/rule/tree/client/model/res/RuleTreeResultVO.java
@@ -13,9 +13,13 @@ public class RuleTreeResultVO {
     @Data
     public static class RuleTreePathVO {
         private String id;
+        private String op;
+        private String pid;
+        private String type;
         private String name;
         private String param;
         private Boolean result;
+        private List<String> val;
         private RuleTreePathVO son;
         private List<RuleTreePathVO> brothers;
     }
